86ec2cef3336c2d46d3f63d2d83c35306fe9d8ac
zv-dev <zvivien17@gmail.com>
2025-12-12 09:32:36 +0100

    zv-dev: Convert to ESM module format
    - Change module output from CommonJS to ES2020
    - Add "type": "module" to package.json
    - Required for Vite workspace compatibility

--- dist/api.types.js
+++ dist/api.types.js
@@ -1,8 +1,7 @@
-"use strict";
 /**
  * Unified API Response Types
  *
  * Standard response structure for all API endpoints.
  * Synchronized with backend apiResponse.ts utilities.
  */
-Object.defineProperty(exports, "__esModule", { value: true });
+export {};
--- dist/common.types.js
+++ dist/common.types.js
@@ -1,10 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_WILDCARDS = void 0;
 /**
  * Wildcard user constants
  */
-exports.USER_WILDCARDS = {
+export const USER_WILDCARDS = {
     REGISTERED_USER: '*:registered',
     NON_REGISTERED_USER: '*:non-registered'
 };
